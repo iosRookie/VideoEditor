dad658fd6abfda4f799f2e79bb1c283049067882
iosRookie <444860018@qq.com>
2019-05-29 18:24:35 +0800

    代码提交

--- VideoEditor/Tools/VideoReverse.swift
+++ VideoEditor/Tools/VideoReverse.swift
@@ -28,12 +28,57 @@ class VideoReverse {
             if CMTimeRangeContainsTimeRange(videoTrack.timeRange, otherRange: timeRange) {
                 timeRangeArray.append(timeRange as NSValue)
             } else {
+                timeRange = CMTimeRangeMake(start: startTime, duration: CMTimeSubtract(asset.duration, startTime))
+                timeRangeArray.append(timeRange as NSValue)
+            }
+        }
+        
+        var tracks = [AVAssetTrack]()
+        var assets = [AVAsset]()
+        for i in 0..<timeRangeArray.count {
+            var subAsset = AVMutableComposition()
+            var subTrack = subAsset.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)!
+            do {
+                try subTrack.insertTimeRange(timeRangeArray[i] as! CMTimeRange, of: videoTrack, at: startTimeArray[i] as! CMTime)
+                let assetNew:AVAsset = subAsset.copy() as! AVAsset
+                tracks.append(assetNew.tracks(withMediaType: .video).last!)
+                assets.append(assetNew)
+            } catch {
                 
             }
         }
         
+        var totalReader:AVAssetReader = try! AVAssetReader(asset: asset)
+        let outputSetting = [kCVPixelBufferPixelFormatTypeKey as String:kCVPixelFormatType_420YpCbCr8BiPlanarFullRange]
+        var totalReaderOutput:AVAssetReaderOutput = AVAssetReaderTrackOutput(track: videoTrack, outputSettings: outputSetting)
+        if totalReader.canAdd(totalReaderOutput) {
+            totalReader.add(totalReaderOutput)
+        }
+        totalReader.startReading()
+        var sampleTimes:Array = [CMSampleBuffer]()
         
+        while let totalSample = totalReaderOutput.copyNextSampleBuffer() {
+            let presentationTime = CMSampleBufferGetPresentationTimeStamp(totalSample)
+//            sampleTimes.append(presentationTime as NSValue)
+        
+        }
         return true
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
